0840f6210d9000c4103af6ca832616415aa5a1c1
沈秋蕙 <o919552512@gmail.com>
2022-09-18 23:45:48 +0800

    - 更新，更換參數。

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const FormAutoFill = new Vue({
     // Google Apps Script 部署為網路應用程式後的 URL
     gas: 'https://script.google.com/macros/s/AKfycbwr_0fM0B5p_pi9wr7ST9G5_04DlE5YOGeOAx6IOqI-7NM8eShkyT9ZT7zFGqoJrTV4/exec',
 
-    id: '',
+    phone: '',
 
     // 避免重複 POST，存資料用的
     persons: {},
@@ -29,48 +29,48 @@ const FormAutoFill = new Vue({
     loading: false
   },
   methods: {
-    // ID 限填 4 碼
+    // ID 限填 10 碼
     limitIdLen(val) {
       if(val.length > 10) {
-        return this.id =  this.id.slice(0, 10);
+        return this.phone =  this.phone.slice(0, 10);
       }
     },
     // 送出表單
     submit() {
       // 再一次判斷是不是可以送出資料
       if(this.person.name !== undefined) {
-        let params = `${this.input.id}=${this.person.id}&${this.input.name}=${this.person.name}&${this.input.gender}=${this.person.gender}&${this.input.phone}=${this.person.phone}&${this.input.site}=${this.person.site}&${this.input.msg}=${this.person.message || '無'}`;
+        let params = `${this.input.phone}=${this.person.phone}&${this.input.name}=${this.person.name}&${this.input.company}=${this.person.company}&${this.input.taxID}=${this.person.taxID}`;
         fetch(this.formAction + '?' + params, {
           method: 'POST'
         }).catch(err => {
             alert('提交成功。');
-            this.id = '';
+            this.phone = '';
             this.person = {};
           })
       }
     }
   },
   watch: {
-    id: function(val) {
-      // ID 輸入到 4 碼就查詢資料
-      if(val.length === 4) {
+    phone: function(val) {
+      // phone 輸入到 10 碼就查詢資料
+      if(val.length === 10) {
 
         // this.persons 裡沒這筆資料，才 POST
-        if(this.persons[this.id] === undefined) {
+        if(this.persons[this.phone] === undefined) {
           this.loading = true;
-          let uri = this.gas + '?id=' + this.id;
+          let uri = this.gas + '?phone=' + this.phone;
           fetch(uri, {
             method: 'POST'
           }).then(res => res.json())
             .then(res => {
-              this.persons[this.id] = res; // 把這次查詢的 id 結果存下來
+              this.persons[this.phone] = res; // 把這次查詢的 phone 結果存下來
               this.person = res;
               this.loading = false;
             })
         }
         // this.persons 裡有資料就吐資料
         else {
-          this.person = this.persons[this.id];
+          this.person = this.persons[this.phone];
         }
 
       }
